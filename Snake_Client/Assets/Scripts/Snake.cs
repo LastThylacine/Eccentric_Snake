3b1a894a86bdb6ef6d91110daed0dc059779531f
LastThylacine <jamalfursov@gmail.com>
2024-03-25 22:41:28 +0300

    Making all with Prefabs

--- Snake_Client/Assets/Scripts/Snake.cs
+++ Snake_Client/Assets/Scripts/Snake.cs
@@ -1,11 +1,24 @@
-using UnityEditor.Rendering;
 using UnityEngine;
 
 public class Snake : MonoBehaviour
 {
+    [SerializeField] private Tail _tailPrefab;
     [SerializeField] private Transform _head;
     [SerializeField] private float _speed = 2f;
     [SerializeField] private float _rotateSpeed = 90f;
+    private Tail _tail;
+
+    public void Init(int detailCount)
+    {
+        _tail = Instantiate(_tailPrefab, transform.position, Quaternion.identity);
+        _tail.Init(_head, _speed, detailCount);
+    }
+
+    public void Destroy()
+    {
+        _tail.Destroy();
+        Destroy(gameObject);
+    }
 
     private void Update()
     {
--- Snake_Client/Assets/Scripts/Tail.cs
+++ Snake_Client/Assets/Scripts/Tail.cs
@@ -3,20 +3,67 @@ using UnityEngine;
 
 public class Tail : MonoBehaviour
 {
-    [SerializeField] private Transform _head;
-    [SerializeField] private List<Transform> _details;
+    [SerializeField] private Transform _detailPrefab;
     [SerializeField] private float _detailDistance = 1f;
-    [SerializeField] private float _snakeSpeed = 2f;
+    private float _snakeSpeed = 2f;
+    private Transform _head;
+    private List<Transform> _details = new List<Transform>();
     private List<Vector3> _positionHistory = new List<Vector3>();
 
-    private void Awake()
+    public void Init(Transform head, float speed, int detailCount)
     {
-        _positionHistory.Add(_head.position);
+        _snakeSpeed = speed;
+        _head = head;
 
-        for (int i = 0; i < _details.Count; i++)
+        _details.Add(transform);
+        _positionHistory.Add(_head.position);
+        _positionHistory.Add(transform.position); 
+
+        SetDetailCount(detailCount);
+    }
+
+    private void SetDetailCount(int detailCount)
+    {
+        if (detailCount == _details.Count + 1) return;
+
+        int diff = (_details.Count - 1) - detailCount;
+
+        if (diff < 1)
         {
-            _positionHistory.Add(_details[i].position);
+            for (int i = 0; i < -diff; i++)
+            {
+                AddDetail();
+            }
         }
+        else
+        {
+            for (int i = 0; i < diff; i++)
+            {
+                RemoveDetail();
+            }
+        }
+    }
+
+    private void AddDetail()
+    {
+        Vector3 position = _details[_details.Count - 1].position;
+        Transform detail = Instantiate(_detailPrefab, position, Quaternion.identity);
+        _details.Insert(0, detail);
+        _positionHistory.Add(position);
+    }
+
+    private void RemoveDetail()
+    {
+        if (_details.Count <= 1)
+        {
+            Debug.LogError("ѕытаемс€ удалить деталь, которой нет");
+            return;
+        }
+
+        Transform detail = _details[0];
+        _details.Remove(detail);
+        Destroy(detail.gameObject);
+        _positionHistory.RemoveAt(_positionHistory.Count - 1);
     }
 
     private void Update()
@@ -37,8 +84,16 @@ public class Tail : MonoBehaviour
         {
             _details[i].position = Vector3.Lerp(_positionHistory[i + 1], _positionHistory[i], distance / _detailDistance);
 
-            Vector3 direction = (_positionHistory[i] - _positionHistory[i + 1]).normalized;
-            _details[i].position += direction * Time.deltaTime * _snakeSpeed;
+            //Vector3 direction = (_positionHistory[i] - _positionHistory[i + 1]).normalized;
+            //_details[i].position += direction * Time.deltaTime * _snakeSpeed;
+        }
+    }
+
+    public void Destroy()
+    {
+        for (int i = 0; i < _details.Count; i++)
+        {
+            Destroy(_details[i].gameObject);
         }
     }
 }
